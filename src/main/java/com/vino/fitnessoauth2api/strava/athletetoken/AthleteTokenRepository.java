c9887a6b3e7de6c5794af77f8b3dff31ef9a1c1d
Vinoth <vinoth186@gmail.com>
2021-02-14 10:05:17 +0530

    Initial Code

---
+++ src/main/java/com/vino/fitnessoauth2api/strava/athletetoken/AthleteTokenRepository.java
@@ -0,0 +1,10 @@
+package com.vino.fitnessoauth2api.strava.athletetoken;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface AthleteTokenRepository
+    extends JpaRepository<AthleteToken, Long> {
+
+}
